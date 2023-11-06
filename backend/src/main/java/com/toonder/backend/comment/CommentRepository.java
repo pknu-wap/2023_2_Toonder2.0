16266de82d7b9879f1f5c5a1602ecb2070a79703
domani0814 <domani_0814@icloud.com>
2023-11-06 16:29:18 +0900

    modfied

--- backend/src/main/java/com/toonder/backend/comment/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toonder.backend.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository <Comment, Integer> {
-    
-}
--- backend/src/main/java/com/toonder/backend/review/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toonder.backend.review;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer>{
-    
-}
