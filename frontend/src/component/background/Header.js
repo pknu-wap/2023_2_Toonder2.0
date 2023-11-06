0c6b4f68da2b6a4b7968aeee43a0380ac9408cb8
ash <hyeonsian23@gmail.com>
2023-11-06 16:24:30 +0900

    프론트엔드 작업 추가

---
+++ frontend/src/component/background/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Head } from "../../styles/background/Head";
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: 24px;
+  color: #efefef;
+  letter-spacing: 2px;
+  margin: 10px;
+`;
+
+const Subtitle = styled.div`
+  font-size: 14px;
+  color: #efefef;
+  letter-spacing: "2px";
+`;
+
+function Header(props) {
+  return (
+    <Head>
+      <Link to="/">
+        <img
+          src={process.env.PUBLIC_URL + "/logo.png"}
+          alt="logo"
+          width="80px"
+          height="auto"
+        />
+      </Link>
+      <Title>{props.title}</Title>
+      <Subtitle>{props.subtitle}</Subtitle>
+    </Head>
+  );
+}
+
+export default Header;
---
+++ frontend/src/component/login/KakaoLoginButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { kakaoClientId, kakaoRedirectUri } from "../../loginInfo";
+
+const loginUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}\
+&redirect_uri=${kakaoRedirectUri}&response_type=code`;
+
+const KakaoLoginButton = () => {
+  return (
+    <div style={{ margin: "30px" }}>
+      <a href={loginUrl} rel="noopener noreferrer">
+        <img
+          src={process.env.PUBLIC_URL + "/kakaoLoginImg.png"}
+          alt="kakaoLoginImg"
+          width="50px"
+          height="auto"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default KakaoLoginButton;
