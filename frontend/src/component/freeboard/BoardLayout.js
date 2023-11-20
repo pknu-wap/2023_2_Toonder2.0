bada328a9b5dea560dd98babdaf521e93ad0a18d
ash <hyeonsian23@gmail.com>
2023-11-20 23:26:54 +0900

    refactor : Outlet 사용해 Board 공통 레이아웃 지정

---
+++ frontend/src/component/freeboard/BoardLayout.js
@@ -0,0 +1,79 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Header from "../background/Header";
+import { useMatch } from "react-router-dom";
+
+const BoardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 540px;
+  margin: 0 auto;
+  margin-bottom: 30px; // Footer와의 간격
+  text-align: center;
+  align-items: center;
+  transition: box-shadow 0.3s ease;
+  overflow-x: hidden;
+  overflow-y: hidden;
+  min-height: 100vh;
+  /* 미디어 쿼리 추가 */
+  @media (max-width: 540px) {
+    max-width: 100%; /* 모바일 화면에서 가로 길이를 100%로 설정 */
+  }
+`;
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1.5px solid #d8d8d8;
+  border-radius: 10px;
+  box-sizing: border-box;
+  width: 500px;
+  margin-top: 10px;
+  padding: 10px 20px 10px 20px; /* 내부 여백 */
+  /* 미디어 쿼리 추가 */
+  @media (max-width: 540px) {
+    width: 96vw;
+  }
+`;
+
+const WriteBtn = styled.button`
+  font-family: "NIXGONM-Vb";
+  background-color: #6e6e6e;
+  border: none;
+  color: white;
+  font-size: 14px;
+  cursor: pointer;
+  border-radius: 10px;
+  padding: 8px 24px;
+  display: center;
+  transition: box-shadow 0.3s ease;
+
+  &:hover {
+    box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+  }
+
+  margin-left: auto;
+`;
+
+function BoardLayout() {
+  const showWriteBtn = useMatch("/board"); // 현재 경로가 '/board'인지 확인
+
+  return (
+    <>
+      <Link to="/board" style={{ textDecoration: "none" }}>
+        <Header title="자유게시판" />
+      </Link>
+
+      <BoardContainer>
+        {showWriteBtn && <WriteBtn>쓰기</WriteBtn>}
+        <ListContainer>
+          <Outlet />
+        </ListContainer>
+      </BoardContainer>
+    </>
+  );
+}
+
+export default BoardLayout;
