4b3ddc038963fbad550b67d88c2a9d34e3d98d5a
ash <hyeonsian23@gmail.com>
2023-11-21 02:53:59 +0900

    feat : 글 조회 페이지 구현

--- frontend/src/component/freeboard/PostView.js
+++ frontend/src/component/freeboard/PostView.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import Header from "../background/Header";
 import styled from "styled-components";
-import Pagination from "./Pagination";
+import { BoardBtn } from "./BoardLayout";
 
-const PostContainer = styled.div`
+const PostHeader = styled.div`
   color: #e2e2e2;
   border-bottom: 1px solid #ccc;
   width: 100%;
@@ -12,24 +11,45 @@ const PostContainer = styled.div`
 `;
 
 const PostTitle = styled.div`
-  font-size: 16px;
+  font-size: 20px;
   margin: 20px 0px 0px 0px;
   @media (max-width: 540px) {
     font-size: 16px; /* 모바일에서 글제목 크기 */
   }
-
-  &:hover {
-    cursor: pointer;
-  }
 `;
 
 const PostProperty = styled.div`
+  display: flex; /* Flexbox를 사용하여 내부 아이템 정렬 */
+  justify-content: space-between; /* 내부 아이템 간격 벌리기 */
+  align-items: center; /* 세로 중앙 정렬 */
   color: #d8d8d8;
   font-size: 12px;
   margin: 10px 0px 20px 0;
   @media (max-width: 540px) {
     font-size: 12px; /* 모바일에서 글 속성 크기 */
   }
+`;
+
+const PostActions = styled.div`
+  display: flex; /* Flexbox를 사용하여 내부 아이템 정렬 */
+  align-items: center; /* 세로 중앙 정렬 */
+`;
+
+const PostBtn = styled(BoardBtn)`
+  font-size: 12px;
+  padding: 6px 16px;
+  margin-left: 8px; /* 버튼 사이의 간격 조정 */
+  }
+`;
+
+const PostContent = styled.div`
+  font-size: 14px;
+  color: #e2e2e2;
+  margin: 20px 0px 20px 0px;
+  white-space: pre-line; /* 글 내용 줄바꿈 적용 */
+  line-height: 1.8; /* 줄간격 조정을 위한 line-height 속성 */
+  text-align: left; /* 왼쪽 정렬을 위한 text-align 속성 */
+  border-bottom: 1px solid #ccc;
 `;
 
 function PostView() {
@@ -39,30 +59,49 @@ function PostView() {
   const offset = (page - 1) * limit;
   const navigate = useNavigate();
   const location = useLocation();
+  const { state } = location;
 
-  useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    queryParams.set("p_num", String(page));
-    navigate(`${location.pathname}?${queryParams.toString()}`);
-  }, [page, navigate, location.search]);
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch("./postdata.json");
         const data = await response.json();
-        setJsonData(data);
+        const foundPost = data.find((post) => post.brdNo === state?.brdNo);
+        setPost(foundPost);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [state]);
 
   return (
     <>
-    
+      <PostHeader>
+        {post ? (
+          <>
+            <PostTitle>{post.brdTitle}</PostTitle>
+            <PostProperty>
+              <div>
+                {`${post.brdUpdateDate} · 조회 ${post.brdViewCount} · 좋아요 ${post.brdLike} · 작성자 ${post.member}`}
+              </div>
+              <PostActions>
+                <PostBtn>수정</PostBtn>
+                <PostBtn>삭제</PostBtn>
+              </PostActions>
+            </PostProperty>
+          </>
+        ) : (
+          <p>데이터를 불러오는 중입니다...</p>
+        )}
+      </PostHeader>
+      <PostContent>
+        {post && post.brdContent}
+        <PostBtn>❤</PostBtn>
+      </PostContent>
     </>
   );
 }
