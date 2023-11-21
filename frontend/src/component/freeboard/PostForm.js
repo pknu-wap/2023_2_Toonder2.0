f9a4154c9adf5a13bafccad9f943861d1a984ec6
ash <hyeonsian23@gmail.com>
2023-11-21 21:18:32 +0900

    feat : 글작성 페이지 레이아웃 구현

--- frontend/src/component/freeboard/PostForm.js
+++ frontend/src/component/freeboard/PostForm.js
@@ -1,70 +1,77 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import Header from "../background/Header";
 import styled from "styled-components";
-import Pagination from "./Pagination";
-
-const PostContainer = styled.div`
-  color: #e2e2e2;
-  border-bottom: 1px solid #ccc;
-  width: 100%;
-  text-align: left;
-`;
-
-const PostTitle = styled.div`
-  font-size: 16px;
-  margin: 20px 0px 0px 0px;
-  @media (max-width: 540px) {
-    font-size: 16px; /* 모바일에서 글제목 크기 */
-  }
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const PostProperty = styled.div`
-  color: #d8d8d8;
-  font-size: 12px;
-  margin: 10px 0px 20px 0;
-  @media (max-width: 540px) {
-    font-size: 12px; /* 모바일에서 글 속성 크기 */
-  }
-`;
+import { PostHeader, PostActions, PostProperty, PostBtn } from "./PostView";
 
 function PostForm() {
-  const [jsonData, setJsonData] = useState([]);
-  const [limit, setLimit] = useState(5);
-  const [page, setPage] = useState(1);
-  const offset = (page - 1) * limit;
   const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    queryParams.set("p_num", String(page));
-    navigate(`${location.pathname}?${queryParams.toString()}`);
-  }, [page, navigate, location.search]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("./postdata.json");
-        const data = await response.json();
-        setJsonData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const [title, setTitle] = useState(""); // 사용자가 입력한 제목을 상태로 관리
+  const [content, setContent] = useState(""); // 사용자가 입력한 글 내용을 상태로 관리
 
   return (
     <>
-    
+      <PostHeader>
+        <PostTitle
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="글 제목"
+        />
+      </PostHeader>
+      <PostContent
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="글 내용을 입력하세요"
+      />
+      <PostActions>
+        <PostBtn
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          취소
+        </PostBtn>
+        <PostBtn>저장</PostBtn>
+      </PostActions>
     </>
   );
 }
+
+const PostTitle = styled.input`
+  font-family: "NIXGONM-Vb";
+  color: #e2e2e2;
+  background: none;
+  border: none;
+  font-size: 20px;
+  margin: 20px 0px 10px 0px;
+  width: 100%;
+
+  &:focus {
+    outline: none;
+    color: #e2e2e2;
+  }
+
+  @media (max-width: 540px) {
+    font-size: 16px; /* 모바일에서 글제목 크기 */
+  }
+`;
+
+const PostContent = styled.textarea`
+  font-family: "NIXGONM-Vb";
+  color: #e2e2e2;
+  background: none;
+  border: none;
+  font-size: 16px;
+  margin: 10px 0;
+  width: 100%;
+  line-height: 1.5;
+  min-height: 200px; /* 사용자가 글을 쓸 수 있는 높이를 지정 */
+  resize: none; /* 사용자가 글 작성 영역을 조절하지 못하도록 함 */
+
+  &:focus {
+    outline: none;
+    color: #e2e2e2;
+  }
+`;
 
 export default PostForm;
