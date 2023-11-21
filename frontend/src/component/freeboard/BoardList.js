19cb1ecb94e884ab1765f32a2dd4d7144cad8208
ash <hyeonsian23@gmail.com>
2023-11-21 21:17:42 +0900

    refactor : 불필요한 코드 제거

--- frontend/src/component/freeboard/BoardList.js
+++ frontend/src/component/freeboard/BoardList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
-import Header from "../background/Header";
 import styled from "styled-components";
 import Pagination from "./Pagination";
 
