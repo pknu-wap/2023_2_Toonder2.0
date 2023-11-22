aeb8cf78d929182071bc59bd0ccc74c1d3037bb8
ash <hyeonsian23@gmail.com>
2023-11-23 01:09:08 +0900

    fix : 일부 요소에 사이드바가 가려지는 현상 수정

--- frontend/src/component/background/Sidebar.js
+++ frontend/src/component/background/Sidebar.js
@@ -13,6 +13,7 @@ const SidebarContainer = styled.div`
   height: 100%;
   background-color: rgba(30, 30, 30, 0.95);
   transition: right 0.3s ease;
+  z-index: 1000; /* Set a high z-index */
 `;
 
 const Menu = styled.div`
