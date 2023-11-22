06409bfa3f0e1c94569fe99eb8c2ae3048172365
ash <hyeonsian23@gmail.com>
2023-11-22 18:52:08 +0900

    feat : 글 조회 페이지 레이아웃 완성

--- frontend/src/component/freeboard/PostView.js
+++ frontend/src/component/freeboard/PostView.js
@@ -48,6 +48,7 @@ function PostView() {
 
   return (
     <>
+      {/* 본문 */}
       <PostHeader>
         {post ? (
           <>
@@ -76,25 +77,34 @@ function PostView() {
           </>
         )}
       </PostContentContainer>
+
+      {/* 댓글 */}
       <div style={{ color: "#e2e2e2", textAlign: "left", fontSize: "18px" }}>
         댓글
       </div>
       <CommentContainer>
-        <CommentWrapper>
-          {filteredComments.map((comment) => (
-            <>
-              <div style={{ fontSize: "14px" }} key={comment.cmtNo}>
-                {`${comment.memName} : ${comment.cmtContent}`}
+        {filteredComments.map((comment) => (
+          <>
+            <CommentInnerContainer>
+              <div key={comment.cmtNo}>
+                <CommentContentWrapper style={{ fontSize: "14px" }}>
+                  <div>{`${comment.memName} : ${comment.cmtContent}`}</div>
+                </CommentContentWrapper>
+                <CommentPropertyWrapper>
+                  <span>{`${comment.cmtUpdateDate}`}</span>
+                  <CommentBtn>♥</CommentBtn>
+                  <span>{`${comment.cmtLike}`}</span>
+                  <CommentBtn>수정</CommentBtn>
+                  <CommentBtn>삭제</CommentBtn>
+                </CommentPropertyWrapper>
               </div>
-
-              <CommentActions>
-                <CommentBtn>수정</CommentBtn>
-                <CommentBtn>삭제</CommentBtn>
-              </CommentActions>
-            </>
-          ))}
-        </CommentWrapper>
-        <CommentWriteContainer></CommentWriteContainer>
+            </CommentInnerContainer>
+          </>
+        ))}
+        <CommentWriteFormContainer>
+          <CommentWriteForm placeholder="댓글을 입력하세요"></CommentWriteForm>
+          <CommentSubmitBtn>등록</CommentSubmitBtn>
+        </CommentWriteFormContainer>
       </CommentContainer>
     </>
   );
@@ -137,7 +147,6 @@ export const PostBtn = styled(BoardBtn)`
   font-size: 12px;
   padding: 6px 16px;
   margin-left: 8px; /* 버튼 사이의 간격 조정 */
-  }
 `;
 
 const PostContentContainer = styled.div`
@@ -172,7 +181,7 @@ const CommentContainer = styled.div`
   border-radius: 10px;
   box-sizing: border-box;
   margin-top: 10px;
-  padding: 10px 8px 10px 8px; /* 내부 여백 */
+  padding: 0px 10px 10px 10px; /* 내부 여백 */
   /* 모바일 화면에 맞게 너비 조정 */
   width: calc(100%); /* 좌우 패딩값을 고려하여 너비 조정 */
 
@@ -182,28 +191,78 @@ const CommentContainer = styled.div`
   }
 `;
 
-const CommentWrapper = styled(PostProperty)`
-  margin-botton: 10px;
-  flex-wrap: wrap;
+const CommentInnerContainer = styled.div`
+  margin-top: 10px;
+  border-bottom: 1px solid grey;
 `;
 
-const CommentActions = styled(PostActions)``;
+const CommentContentWrapper = styled(PostProperty)`
+  margin-bottom: 0px;
+`;
+
+const CommentPropertyWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-size: 12px;
+  color: #e2e2e2;
+  margin: 10px 0px 15px 0px;
+`;
 
 const CommentBtn = styled.button`
   font-family: "NIXGONM-Vb";
+  float: left;
   background: none;
   border: none;
   color: #e2e2e2;
   font-size: 14px;
   cursor: pointer;
-  padding: 6px 8px;
-  display: center;
-  margin-left: auto;
+  display: flex;
+  padding: 0px 8px;
   background: none;
   font-size: 12px;
-  padding: 6px 6px;
 `;
 
-const CommentWriteContainer = styled.div``;
+const CommentWriteFormContainer = styled.div`
+  position: relative;
+  width: 100%;
+  margin-top: 30px;
+  margin-bottom: 10px; /* 버튼과의 간격 조정 */
+`;
+
+const CommentWriteForm = styled.textarea`
+  font-family: "NIXGONM-Vb";
+  display: flex;
+  border: 1.5px solid #808080;
+  font-size: 14px;
+  color: #efefef;
+  background: #808080;
+  width: 100%;
+  border-radius: 10px;
+  box-sizing: border-box;
+  margin: 20px 0px 10px 0px;
+  padding: 10px 10px 50px 10px; /* 내부 여백 */
+  resize: none;
+
+  &:focus {
+    outline: none;
+    color: #efefef;
+  }
+
+  &::placeholder {
+    color: #efefef;
+  }
+`;
+
+const CommentSubmitBtn = styled.button`
+  font-family: "NIXGONM-Vb";
+  width: 100%; 
+  padding: 8px 16px;
+  background-color: #808080;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 export default PostView;
