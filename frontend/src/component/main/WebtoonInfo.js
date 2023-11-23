5c26cfb6a36f0c7885ef2dcbdb534440873e07ad
ash <hyeonsian23@gmail.com>
2023-11-23 23:17:48 +0900

    feat : Webtoon Info 페이지 추가

---
+++ frontend/src/component/main/WebtoonInfo.js
@@ -0,0 +1,19 @@
+import Header from "../background/Header";
+import Navbar from "../background/Navbar";
+import styled from "styled-components";
+
+const ContentWrapper = styled.div`
+
+`;
+
+
+function WebtoonInfo() {
+  return (
+    <>
+    <Navbar />
+      <Header title="웹툰 정보"/>
+    </>
+  );
+}
+
+export default WebtoonInfo;
