131924e1cb175177aef0db600aea223613da4e95
ash <hyeonsian23@gmail.com>
2023-11-24 16:27:46 +0900

    feat : 별점 매기기 기능 구현

--- frontend/src/component/main/HoverRating.js
+++ frontend/src/component/main/HoverRating.js
@@ -1,37 +1,49 @@
-import * as React from 'react';
-import Rating from '@mui/material/Rating';
-import Box from '@mui/material/Box';
-import StarIcon from '@mui/icons-material/Star';
+import * as React from "react";
+import Rating from "@mui/material/Rating";
+import Box from "@mui/material/Box";
+import StarIcon from "@mui/icons-material/Star";
 
-export default function HoverRating({ value: propValue, readOnly, size }) {
-    const [localValue, setLocalValue] = React.useState(propValue);
-    const [hover, setHover] = React.useState(-1);
-  
-    return (
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
+export default function HoverRating({
+  value: propValue,
+  readOnly,
+  size,
+  onChange,
+}) {
+    
+  const [localValue, setLocalValue] = React.useState(propValue);
+  const [hover, setHover] = React.useState(-1);
+
+  // 별점 변경 시 상위 컴포넌트로 변경된 값을 전달하는 함수
+  const handleRatingChange = (newValue) => {
+    setLocalValue(newValue); // 로컬 상태 업데이트
+    onChange(newValue); // 부모 컴포넌트의 상태 업데이트
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Rating
+        name="hover-feedback"
+        value={localValue}
+        precision={0.5}
+        size={size}
+        readOnly={readOnly}
+        onChange={(event, newValue) => {
+          if (!readOnly) {
+            handleRatingChange(newValue); // 변경된 값을 처리하는 함수 호출
+          }
         }}
-      >
-        <Rating
-          name="hover-feedback"
-          value={localValue}
-          precision={0.5}
-          size={size} // 여기서 props로 전달된 size를 사용합니다.
-          readOnly={readOnly}
-          onChange={(event, newValue) => {
-            if (!readOnly) {
-              setLocalValue(newValue);
-            }
-          }}
-          onChangeActive={(event, newHover) => {
-            if (!readOnly) {
-              setHover(newHover);
-            }
-          }}
-          emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-        />
-      </Box>
-    );
-  }
+        onChangeActive={(event, newHover) => {
+          if (!readOnly) {
+            setHover(newHover);
+          }
+        }}
+        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+      />
+    </Box>
+  );
+}
