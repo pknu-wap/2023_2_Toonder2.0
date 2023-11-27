2d000aa0e12f372f8ea9cd6f81f83b5aa62b3fc3
ash <hyeonsian23@gmail.com>
2023-11-28 02:41:20 +0900

    feat : 테마별 색상 지정

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -22,11 +22,12 @@ export const GlobalStyles = createGlobalStyle`
 
 export const lightTheme = {
   font: "NIXGONM-Vb", // 폰트 추가
-  header: "#424242",
+  header: "#212121",
   body: "#fafafa", // 전체 브라우저의 색상을 green으로 지정
   footerBackground: "#bdbdbd",
   footerText: "#212121",
   text: "#212121",
+  icon: "#212121", // Navbar 아이콘
   btnBackground: "#212121",
   btnBackgroundDisabled: "#616161", // 회원가입 페이지 입력 폼 유효성 미통과시
   containerBorder: "#9e9e9e", // 테두리
@@ -45,6 +46,7 @@ export const darkTheme = {
   header: "#efefef",
   body: "#414141",
   text: "#efefef",
+  icon: "white", 
   btnBackground: "#6e6e6e",
   btnBackgroundDisabled: "#6e6e6e66",
   footerBackground: "#121212",
