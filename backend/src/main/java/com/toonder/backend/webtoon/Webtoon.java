0accab4de77bebf72f84960b441fb7409dba3710
JH <wngml0708@gmail.com>
2023-11-29 13:38:27 +0900

    메인페이지 outline 추천+검색/리스트 19금 필터 적용

--- backend/src/main/java/com/toonder/backend/webtoon/Webtoon.java
+++ backend/src/main/java/com/toonder/backend/webtoon/Webtoon.java
@@ -19,26 +19,27 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "webtoon")
+@Table(name = "wbtnInfo")
 public class Webtoon {
+
     @Id
     @Column(name = "mastrId", nullable = false)
     private String mastrId; //자료고유ID
 
-    @Column(name = "title", nullable = false)
     private String title; //작품명
 
     private String pictrWritrNm; //그림작가
-    private String sntncWritrNm; //글작가
     private String mainGenreCdNm; //대표장르코드명
 
-    @Column(length = 3000)
+    //@Column(length = 3000)
     private String outline; //줄거리
 
     private String pltfomCdNm; //플랫폼코드명
-    private String fnshYn; //완결여부
-    private String webtoonPusryYn; //웹툰연재여부	
+
     private String imageDownloadUrl; //이미지다운로드URL(표지)
+    private String adult;
+    private String outline_recommendations;
+
 
     @OneToMany(mappedBy = "webtoon", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	@JsonBackReference
