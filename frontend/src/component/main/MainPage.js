eaec494d59b430dd67dffdf90b0acdbb55a5059d
wkdghdwns199 <wkdghdwns199@naver.com>
2023-11-29 15:57:40 +0900

    feat : 메인 페이지 제목 생성

--- frontend/src/component/main/MainPage.js
+++ frontend/src/component/main/MainPage.js
@@ -2,6 +2,33 @@ import Header from "../background/Header";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import supabase from "../supabase";
+import styled from "styled-components";
+
+const RecommandWebtoonArea = styled.div`
+  overflow : hidden;
+  scroll : yes;
+  margin-top : 20px;
+
+  width : 80%;
+`
+
+const Subtitle = styled.div`
+  margin-top : 20px;
+  font-size: 18px;
+  color: #efefef;
+  letter-spacing: "2px";
+  border-bottom: 1px solid white ;
+  padding : 10px;
+  width : 60%;  
+`;
+
+
+const DivTitle = styled.div`
+  text-align : left;
+  margin-top:20px;
+  font-size :15px;
+  color: #efefef;
+`
 
 function MainPage() {
   // 접속한 사용자 이메일 불러오기
@@ -37,6 +64,16 @@ function MainPage() {
   return (
     <>
       <Header title="TOONDER" subtitle="당신이 찾는 모든 웹툰에 대해" />
+
+      <Subtitle>오늘 ??? 님이 볼만한 웹툰!</Subtitle>
+
+      <RecommandWebtoonArea>
+        <DivTitle>좋아하시는 장르가 비슷해요.</DivTitle>
+        <DivTitle>좋아하시는 그림체가 비슷해요.</DivTitle>
+        <DivTitle>좋아하시는 줄거리가 비슷해요.</DivTitle>
+        <DivTitle>좋아하시는 작가님의 다른 작품이에요.</DivTitle>
+      </RecommandWebtoonArea>
+
     </>
   );
 }
