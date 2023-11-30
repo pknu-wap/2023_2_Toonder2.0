36031d58a087d6a79291c52d46465cb70bea9cf8
박연수 <kite@bag-yeonsuui-MacBookPro.local>
2023-11-30 19:56:01 +0900

    feat/매일 밤 0시0분 마다 돌아가게 함 + DB 오류 제거

--- ai/toonder_module/create_db.py
+++ ai/toonder_module/create_db.py
@@ -1,8 +1,6 @@
 import csv
 from toonder_db_connection import connect_to_db
 
-url = 'ai/data/data.csv'
-
 def create_db(url):
     db, cursor, api_url, api_key = connect_to_db()
 
@@ -18,23 +16,28 @@ def create_db(url):
             print("successfully DROPED")
             
             # 테이블 생성
-            # create_table_query = f"""
-            # CREATE TABLE IF NOT EXISTS wbtnInfo (
-            #     mastrId VARCHAR(255) NOT NULL,
-            #     title VARCHAR(255) NOT NULL,
-            #     pictrWritrNm VARCHAR(255) CHARACTER SET utf8mb4,
-            #     mainGenreCdNm VARCHAR(255) CHARACTER SET utf8mb4,
-            #     outline VARCHAR(3000) CHARACTER SET utf8mb4,
-            #     pltfomCdNm VARCHAR(255) CHARACTER SET utf8mb4,
-            #     imageDownloadUrl VARCHAR(255) CHARACTER SET utf8mb4,
-            #     adult VARCHAR(255) CHARACTER SET utf8mb4,
-            #     drawId VARCHAR(255) CHARACTER SET utf8mb4,
-            #     outline_recommendations VARCHAR(3000) CHARACTER SET utf8mb4
-            # ) CHARACTER SET utf8mb4;
-            # """
+            create_table_query = f"""
+            CREATE TABLE IF NOT EXISTS wbtnInfo (
+                mastrId VARCHAR(255) NOT NULL,
+                title VARCHAR(255) NOT NULL,
+                pictrWritrNm VARCHAR(255) CHARACTER SET utf8mb4,
+                mainGenreCdNm VARCHAR(255) CHARACTER SET utf8mb4,
+                outline VARCHAR(3000) CHARACTER SET utf8mb4,
+                pltfomCdNm VARCHAR(255) CHARACTER SET utf8mb4,
+                imageDownloadUrl VARCHAR(255) CHARACTER SET utf8mb4,
+                adult VARCHAR(255) CHARACTER SET utf8mb4,
+                drawId VARCHAR(255) CHARACTER SET utf8mb4,
+                outline_recommendations VARCHAR(3000) CHARACTER SET utf8mb4
+            ) CHARACTER SET utf8mb4;
+            """
+            
+            
+            # set_primary_key=f"alter table wbtnInfo add primary key(mastrId);"
+            # cursor.execute(set_primary_key)
+            # print("successfully set primary key")
+
             create_table_query=f"""            
             ALTER TABLE wbtnInfo
-            
             MODIFY COLUMN pictrWritrNm VARCHAR(255) CHARACTER SET utf8mb4,
             MODIFY COLUMN mainGenreCdNm VARCHAR(255) CHARACTER SET utf8mb4,
             MODIFY COLUMN outline VARCHAR(3000) CHARACTER SET utf8mb4,
@@ -51,7 +54,15 @@ def create_db(url):
             insert_query = f"INSERT INTO wbtnInfo ({', '.join(headers)}) VALUES ({', '.join(['%s'] * len(headers))})"
             
             for row in reader:
-                cursor.execute(insert_query, row)
+                try:
+                    cursor.execute(insert_query, row)
+                    # 에러가 없으면 데이터베이스에 커밋
+                    db.commit()
+                except Exception as e:
+                    # 에러가 발생했을 때 출력하고 넘어감
+                    print(f"Error occurred: {e}")
+                    db.rollback()  # 롤백하여 트랜잭션 취소
+                    continue  # 다음 루프로 이동
             print("successfully INSERTED")
 
         db.commit()
@@ -62,5 +73,3 @@ def create_db(url):
 
     except Exception as e:
         print(f"오류 발생: {str(e)}")
-        
-create_db(url) # db에 연결
--- ai/toonder_module/main.py
+++ ai/toonder_module/main.py
@@ -1,51 +1,47 @@
 import shutil
 import os
+import schedule
+import time
 from api_to_csv import api_to_csv
 from csv_to_data import csv_to_data
 from toonder_db_connection import connect_to_db
 from create_db import create_db
 from data_to_img import data_to_img
-# python3.11 환경에서 돌릴 것!!
-# python3.11 환경에서 돌릴 것!!
-# python3.11 환경에서 돌릴 것!!
-# python3.11 환경에서 돌릴 것!!
-# python3.11 환경에서 돌릴 것!!
-# python3.11 환경에서 돌릴 것!!
-global directory
-directory = "ai/data"
-import os
+from toonder_csv import toonder_csv
+from toonder_img import toonder_img
 
-directory = 'ai/data/toonder_img/class'
+def main():
 
-# 디렉토리가 존재하든 안하든 일단 생성
-def delete_data_folder():
-    
-    if os.path.exists(directory):
-        shutil.rmtree(directory)
-        os.makedirs(directory)
-        print(f"폴더 '{directory}'와 하위 디렉토리가 모두 삭제되었습니다.")    
-        print(f"디렉토리 {directory}가 생성되었습니다.")
+    url = 'ai/data/data.csv'
+    db, cursor, api_url, api_key = connect_to_db()
 
-    else:
-        os.makedirs(directory)
-        print(f"디렉토리 {directory}가 생성되었습니다.")
-
-url = 'ai/data/data.csv'
-db, cursor, api_url, api_key = connect_to_db()
-
-# api_to_csv() # api -> csv
-# print("api_to_csv() done")
-# csv_to_data() # csv에서 중복 제거
-# print("csv_to_data() done")
-# data_to_img(url) # csv의 이미지 url에서 jpeg로 파일 변경
-# print("data_to_img(url) done")
-# toonder_csv()
-# print("toonder_csv() done")
-# toonder_img()
-# print("toonder_img() done")
-# create_db(url) # db에 연결
-# -----여기까지는 무리 없이 됨-----
+    # api_to_csv() # api -> csv
+    # print("api_to_csv() done")
+    # csv_to_data() # csv에서 중복 제거
+    # print("csv_to_data() done")
+    # data_to_img(url) # csv의 이미지 url에서 jpeg로 파일 변경
+    # print("data_to_img(url) done")
+    # toonder_csv()
+    # print("toonder_csv() done")
+    # toonder_img()
+    # print("toonder_img() done")
+    # create_db(url) # db에 연결
+    print("PROGRAM ENDED")
+    # -----여기까지는 무리 없이 됨-----
 
 
 
-print("PROGRAM ENDED")
+    # 이 함수에 매일 00시 00분에 실행하고자 하는 작업을 넣어주세요.
+    print("매일 00시 00분에 실행되는 작업입니다.")
+
+def job():
+    # main() 함수를 실행하는 함수를 정의합니다.
+    main()
+
+# 스케줄링 설정: 매일 00시 00분에 job() 함수를 실행하도록 설정합니다.
+schedule.every().day.at("00:00").do(job)
+
+while True:
+    # 스케줄링된 작업을 실행합니다.
+    schedule.run_pending()
+    time.sleep(60)  # 60초마다 스케줄을 확인합니다.
